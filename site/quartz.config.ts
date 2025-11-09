5b01a5957c29bd0d35ebfb07f1eb4f86a28bb52c
Skade2050 <leandroperezmartinez5@gmail.com>
2025-11-09 13:01:42 +0100

    Configure Quartz Pages

--- site/quartz.config.ts
+++ site/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "BDA-UT1",
+    pageTitle: "BDA-RA1",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "es-ES",
-    baseUrl: "https://skade2050.github.io/proyecto_bd_ra1",
+    baseUrl: "/proyecto_bd_ra1",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
